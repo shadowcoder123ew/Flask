b76fdc9a52cc56eb5c848e82b55c6131abde3838
shadowcoder123ew <shadowcoder123ew@gmail.com>
2025-06-18 10:49:26 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,16 @@
-from flask import Flask, Response
-import random
-import string
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-def generate_large_file():
-    # تعداد حروف در هر بخش
-    chunk_size = 1024 * 1024  # هر بخش 1 مگابایت
-    num_per_chunk = chunk_size  # هر کاراکتر یک بایت است
+@app.route('/')
+def get_client_info():
+    client_ip = request.remote_addr
+    client_port = request.environ.get('REMOTE_PORT')
     
-    # جنریت حروف تصادفی بزرگ
-    for _ in range(100):  # به طور کلی 100 مگابایت داده تولید می‌شود
-        letters = ''.join(random.choices(string.ascii_letters, k=num_per_chunk))
-        yield letters
-
-@app.route('/generate_file')
-def generate_file():
-    return Response(generate_large_file(), 
-                    content_type='text/plain', 
-                    status=200, 
-                    headers={"Content-Disposition": "attachment;filename=generated_file.txt"})
+    return jsonify({
+        'ip': client_ip,
+        'port': client_port
+    })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
